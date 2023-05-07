6755eaf7b41e2038a7e7f368698bf2d1ab99cf60
Yi Guan <yiguanlee225@gmail.com>
2023-05-07 20:11:14 +0800

    now left home n menu sections
    now left home n menu sections

--- resources/views/menus/service2.blade.php
+++ resources/views/menus/service2.blade.php
@@ -137,59 +137,56 @@
     </nav>
         
 
-<main>
+    <body>
+	<header>
+		<h1>INTI Cafeteria</h1>
+		<p>Delicious food at affordable prices</p>
+	</header>
+
+	<nav>
+		<a href="#">Home</a>
+		<a href="#">Menu</a>
+		<a href="#">Contact</a>
+	</nav>
+    
+	<main>
 
 		<article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+1" alt="Food Item 1">
-			<h2>Chicken Sandwich</h2>
-			<p>Grilled chicken breast on a bed of lettuce and tomato, served on whole wheat bread..</p>
-            <p>Price : RM 0</p>
+			<h2>Sandwiches</h2>
 		</article>
 
 		<article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+2" alt="Food Item 2">
-			<h2>Beef Burger</h2>
-			<p>A juicy beef patty with lettuce, tomato, onion, and pickle, served on a sesame seed bun.</p>
-            <p>Price : RM 0</p>
+			<h2>Burgers</h2>
 		</article>
 
 		<article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+3" alt="Food Item 3">
-            <h2>Chicken Wrap</h2>
-			<p>A tortilla filled with chicken meat, cheese, vegetables, and sauces, rolled up like a burrito.</p>
-            <p>Price : RM 0</p>
+            <h2>Wraps</h2>
         </article>
 
         <article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+4" alt="Food Item 4">
-            <h2>Beef Wrap</h2>
-			<p>A large flour tortilla filled with seasoned ground beef, rice, beans, cheese, and salsa, rolled up and served hot.</p>
-            <p>Price : RM 0</p>
+            <h2>Snacks</h2>
         </article>
 
         <article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+5" alt="Food Item 5">
-            <h2>Fish and Chips</h2>
-			<p>A dish consisting of battered and fried fish fillets served with French fries and tartar sauce.</p>
-            <p>Price : RM 0</p>
+            <h2>Western Food</h2>
         </article>
 
         <article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+6" alt="Food Item 6">
-            <h2>Tom Yam Fried Rice</h2>
-			<p>A dish stir-fried with shrimp, carrots, bell peppers, and onions, and a combination of spices and seasonings.</p>
-            <p>Price : RM 0</p>
+            <h2>Fried Rice</h2>
         </article>
 
         <article>
 			<img src="https://via.placeholder.com/300x200.png?text=Food+Item+7" alt="Food Item 7">
-            <h2>Chicken Chop </h2>
-			<p>A dish consisting of a seasoned and breaded chicken breast that has been pan-fried or grilled until crispy, 
-                and served with a variety of side dishes like fries or potato wedges.</p>
-            <p>Price : RM 14</p>
+            <h2>Noodles</h2>
         </article>
-  </main>
 
+	</main>
 </body>
 
 
